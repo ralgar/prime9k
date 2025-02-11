f32abd42a699866fb4b1759a61cb89d0605b8374
Ryan Algar <me@ralgar.dev>
2025-02-11 14:20:19 -0800

    test(cpu): add tests for CPU compute class

---
+++ tests/test_composites_cpu.py
@@ -0,0 +1,28 @@
+from typing import List
+
+import pytest
+
+from common.generators import generate_composites, generate_random_number
+from compute_cpu import ComputeCPU
+
+
+cpu = ComputeCPU()
+
+
+def setup_data() -> List[int]:
+    data = []
+
+    data.extend(generate_composites(10, 1))
+    data.extend(generate_composites(10, generate_random_number(4)))
+    data.extend(generate_composites(10, generate_random_number(7)))
+    data.extend(generate_composites(10, generate_random_number(10)))
+    data.extend(generate_composites(10, generate_random_number(13)))
+    data.extend(generate_composites(10, generate_random_number(16)))
+    data.extend(generate_composites(10, generate_random_number(19)))
+
+    return data
+
+
+@pytest.mark.parametrize("num", setup_data())
+def test_prime_numbers(num) -> None:
+    assert not cpu.is_prime(num), f"Failed for {num}"
---
+++ tests/test_primes_cpu.py
@@ -0,0 +1,28 @@
+from typing import List
+
+import pytest
+
+from common.generators import generate_primes, generate_random_number
+from compute_cpu import ComputeCPU
+
+
+cpu = ComputeCPU()
+
+
+def setup_data() -> List[int]:
+    data = []
+
+    data.extend(generate_primes(10, 1))
+    data.extend(generate_primes(10, generate_random_number(4)))
+    data.extend(generate_primes(10, generate_random_number(7)))
+    data.extend(generate_primes(10, generate_random_number(10)))
+    data.extend(generate_primes(10, generate_random_number(13)))
+    data.extend(generate_primes(10, generate_random_number(16)))
+    data.extend(generate_primes(10, generate_random_number(19)))
+
+    return data
+
+
+@pytest.mark.parametrize("num", setup_data())
+def test_prime_numbers(num):
+    assert cpu.is_prime(num), f"Failed for {num}"
